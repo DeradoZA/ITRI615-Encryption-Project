1e488a552ae55a2507f65eb8ab08e3019d98ae95
Daniel <danicoetzee1@gmail.com>
2023-04-05 21:43:05 +0200

    Changed Vernam algorithm to work with files

--- Algorithms/Vernam.py
+++ Algorithms/Vernam.py
@@ -1,47 +1,62 @@
 import random
+from PIL import Image
+import PIL
 
-def VernamEncrypt(plainText):
-    plainTextLength = len(plainText)
-    cipherText = ""
-    vernamKey = ""
+
+def VernamEncrypt(plainFileBytes):
+    plainTextLength = len(plainFileBytes)
+    cipherBytes = b""
+    vernamKeyBytes = b""
 
     for index in range(plainTextLength):
-        keyRandomASCIIValue = random.randint(0, 255)
-        keyChar = chr(keyRandomASCIIValue)
-        plainTextASCIIValue = ord(plainText[index])
+        keyByte = bytes([random.randint(0, 255)])
+        plainFileByte = plainFileBytes[index]
 
-        cipherTextASCIIValue = plainTextASCIIValue ^ keyRandomASCIIValue
+        vernamKeyBytes = vernamKeyBytes + keyByte
+        cipherBytes = cipherBytes + bytes([plainFileByte ^ keyByte[0]])
 
-        vernamKey = vernamKey + keyChar
-        cipherText = cipherText + chr(cipherTextASCIIValue)
+    return cipherBytes, vernamKeyBytes
 
-    return cipherText, vernamKey
 
 def VernamDecrypt(cipherText, vernamKey):
-    plainTextLength = len(cipherText)
-    decryptedText = ""
-    
-    for index in range(plainTextLength):
-        cipherTextASCIIValue = ord(cipherText[index])
-        vernamKeyTextASCIIValue = ord(vernamKey[index])
+    cipherTextLength = len(cipherText)
+    decryptedBytes = b""
 
-        decryptedASCIIValue = cipherTextASCIIValue ^ vernamKeyTextASCIIValue
+    for index in range(cipherTextLength):
+        cipherByte = cipherText[index]
+        vernamKeyByte = vernamKey[index]
 
-        decryptedText = decryptedText + chr(decryptedASCIIValue)
-        
-    return decryptedText
+        decryptedByte = cipherByte ^ vernamKeyByte
+
+        decryptedBytes = decryptedBytes + bytes([decryptedByte])
+
+    return decryptedBytes
+
 
 if __name__ == "__main__":
-    plainText = input(
-        "Input the plaintext you would like to encrypt with vernam cipher: ")
-    
-    cipherText, vernamKey = VernamEncrypt(plainText)
-    
-    print(cipherText)
-    
-    decryptedText = VernamDecrypt(cipherText, vernamKey)
-    
-    print(decryptedText)
 
+    file = open("../Testingfiles/Test8.png", "rb")
 
-    
+    fileBytes = file.read()
+
+    with Image.open("../Testingfiles/Test8.png") as normalImage:
+        normalImage.show()
+
+    cipherBytes, vernamKeyBytes = VernamEncrypt(fileBytes)
+
+    try:
+        with open("EncryptedFile.png", "wb") as encryptedFile:
+            encryptedFile.write(cipherBytes)
+
+        with Image.open("EncryptedFile.png") as encryptedImage:
+            encryptedImage.show()
+    except PIL.UnidentifiedImageError:
+        print("The file is encrypted and cannot be read properly.")
+
+    decryptedBytes = VernamDecrypt(cipherBytes, vernamKeyBytes)
+
+    with open("DecryptedFile.png", "wb") as decryptedFile:
+        decryptedFile.write(decryptedBytes)
+
+    with Image.open("DecryptedFile.png") as decryptedImage:
+        decryptedImage.show()
