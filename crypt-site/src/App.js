90fa7309d1cdb635207e69180d02ab237244eddf
Daniel Coetzee <danicoetzee1@gmail.com>
2023-04-30 23:59:54 +0200

    Finished initial design

--- crypt-site/src/App.js
+++ crypt-site/src/App.js
@@ -1,17 +1,28 @@
 import Navbar from "./Navbar";
-import Form from "./Form";
+import FileForm from "./FileForm";
 import Output from "./Output";
 import Header from "./Header";
+import TextForm from "./TextForm";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 function App() {
 
   return (
-    <div className="app">
-      <Header />
-      <Navbar />
-      <Form />
-      <Output />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Navbar />
+        <Switch>
+          <Route path="/text">
+            <TextForm />
+          </Route>
+          <Route path="/files">
+            <FileForm />
+          </Route>
+        </Switch>
+        <Output />
+      </div>
+    </Router>
   );
 }
 
--- crypt-site/src/Output.js
+++ crypt-site/src/Output.js
@@ -1,9 +1,27 @@
+import { useLocation } from "react-router-dom";
+
 function Output(){
-    return(
-        <div className="output">
-            <h1>This is the output section</h1>
-        </div>
-    );
+    const location = useLocation();
+    const currentPath = location.pathname;
+
+    console.log(currentPath);
+    if (currentPath === "/text"){
+        return(
+            <div className="output">
+                <label for="Output">Result</label>
+                <textarea id="Output" rows='4' columns='100'></textarea>
+            </div>
+        );
+    }
+    else{
+        return(
+            <div className="output">
+                <h1>Output</h1>
+                <a href="/filedownload">Download File</a>
+            </div>
+        );
+    }
+    
 }
 
 export default Output;
